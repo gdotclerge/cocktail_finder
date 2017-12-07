637426d594f0ad881d005043c4567c6c17053cae
Garry <clergeg@gmail.com>
2017-12-07 18:15:14 -0500

    done

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,3 @@
 class Drink < ActiveRecord::Base
-  has_many :user_drinks
-  has_many :users, through: :user_drinks
-  has_many :drink_ingredients
-  has_many :ingredients, through: :drink_ingredients
+
 end
